82731e3d0c16cd3cd1b57c0fcb7efb2f0f81d37b
lyradix <daryl.stpaul@gmail.com>
2024-09-22 22:17:33 +0200

    json server created en localhost

--- live-event/src/ConcertList.js
+++ live-event/src/ConcertList.js
@@ -1,16 +1,16 @@
-const ConcertList = ({concerts, artist,jour,date,handleDelete}) => {
+const ConcertList = ({concerts, artist,jour,date}) => {
     
 
     return ( 
     <div className="concert">
         <h2>{artist}</h2>
-        <button>Click</button>
+        {/* <button>Click</button> */}
         {concerts.map((concert) => (
-            <div className="concert.preview" key = {concert.id}>                
+            <div className="concert-preview" key = {concert.id}>                
             <h2>{concert.jour}</h2>
             <p>{concert.date}</p>
             <p>{concert.artist}</p>
-            <button onClick={() => handleDelete (concert.id)}>Plus d'infos</button>      
+            {/* <button onClick={() => handleDelete (concert.id)}>Plus d'infos</button>       */}
                   
             </div>
         ))}
