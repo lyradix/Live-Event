3a631509b15a90ea2e465ef496b4acb3a9f1c60d
lyradix <daryl.stpaul@gmail.com>
2025-12-01 11:53:33 +0100

    link to partners image modified

--- src/composants/home/Home.jsx
+++ src/composants/home/Home.jsx
@@ -4,61 +4,28 @@ import { Link } from 'react-router-dom';
 import FetchData from '../fetchData';
 import Descriptif from './descriptif';
 import Url from '../../url';
-
 const Home = () => {
-
     const url = Url();
     const {data:partners} = FetchData(`${url}/partners`); 
-    // const {data:concerts, pending, error} = FetchData('https://cornflowerblue-otter-463564.hostingersite.com/concert'); 
-    const {data:info} = FetchData(`${url}/info`); 
-    
-
+    const {data:info} = FetchData(`${url}/info`);  
     const partnersList = partners && <MainPartners partners={partners.filter((partner)=>partner.frontPage === true)} />
     const Descrip = info && <Descriptif info={info.filter((info)=>info.type === "mainInfo")} />
-    // const carouselList = concerts && <NewCarousel concerts={concerts.filter((concert)=> concert.sceneFK.nom === 'eXtravaganza !')} scene = "filtre pour jour"/>
-
-
        return ( 
-
-
         <div className="Home">
-          
-        <div className='programmationBox p-0 m-0'>
-        <h1 className='programmationText'>Programmation</h1>
-        </div>
-        {/* <Poster /> */}
-
-        {/* {carouselList} */}
-        <div className='carousel-preview'>
-        <NewCarousel/>
-        </div>
- 
-       
+            <div className='programmationBox p-0 m-0'>
+                <h1 className='programmationText'>Programmation</h1>
+            </div>
+            <div className='carousel-preview'>
+                <NewCarousel/>
+            </div>
         <Link to="/Parteraires"><h2 className='textPartners'>Partenaires</h2></Link>
         {partnersList}
-
-
-        {/* <div className="partners">
- 
-           <button className='btnPartner'><Link className="nav-link" to="/Artist"><img></img></Link></button>
-           <button  className='btnPartner'><img className="imgPartner" src={imageA} alt="Mobalpa"></img></button>
-           <button  className='btnPartner'><img></img></button>
-           <button  className='btnPartner'><img></img></button>
-           <button  className='btnPartner'><img></img></button>
-           <button  className='btnPartner'><img></img></button>
-           <button  className='btnPartner'><img></img></button>
-           <button  className='btnPartner'><img></img></button>
-   
-           </div> */}
-
             <div>
-            <h2 className='descriptifTitle'>Qui nous sommes...</h2>
-                <article className="descriptif">  
-                {Descrip}
-                </article>
-        </div>
-        
-        
+                <h2 className='descriptifTitle'>Qui nous sommes...</h2>
+                    <article className="descriptif">  
+                        {Descrip}
+                    </article>
+            </div>      
         </div>
     );
 }
